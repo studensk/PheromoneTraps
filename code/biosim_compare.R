437ae7b22c8c8256d2e260e561840b897ffa1543
Kala Studens <kala.studens@gmail.com>
2023-02-08 14:37:11 -0500

    Update plotting code

--- code/biosim_compare.R
+++ code/biosim_compare.R
@@ -19,8 +19,8 @@ all.traps.lst <- lapply(2018:2022, function(yr) {
   return(data)
 })
 all.traps <- bind_rows(all.traps.lst)
-loc.ord.df <- all.traps.lst[[1]]
-loc.ord <- loc.ord.df[order(loc.ord.df$Latitude, decreasing = FALSE),]$Location
+loc.ord.df <- all.traps
+loc.ord <- unique(loc.ord.df[order(loc.ord.df$Latitude, decreasing = FALSE),]$Location)
 all.traps$Location <- factor(all.traps$Location, levels = loc.ord)
 
 captures.lst <- lapply(1:length(all.traps.lst), function(i) {
@@ -37,33 +37,39 @@ captures.lst <- lapply(1:length(all.traps.lst), function(i) {
 captures.df <- bind_rows(captures.lst)
 captures.df$Location <- factor(captures.df$Location, levels = loc.ord)
 
-pheno.lst <- lapply(1:nrow(all.traps), function(r) {
-  row <- all.traps[r,]
-  gen.w <- generateWeather(modelNames = 'Spruce_Budworm_Biology',
-                           fromYr = row$Year, toYr = row$Year, id = row$Location,
-                           latDeg = row$Latitude, longDeg = row$Longitude)
-  gw <- gen.w[[1]]
-  dates <- sapply(1:nrow(gw), function(rw) {
-    row2 <- gw[rw,]
-    year <- row2$Year
-    month <- str_pad(as.character(row2$Month), width = 2, pad = '0', side = 'left')
-    day <- str_pad(as.character(row2$Day), width = 2, pad = '0', side = 'left')
-    date <- paste(c(year, month, day), collapse = '-')
+pheno.yr.lst <- lapply(1:length(all.traps.lst), function(i) {
+  traps <- all.traps.lst[[i]]
+  yr <- (2018:2022)[i]
+  pheno.lst <- lapply(1:nrow(traps), function(r) {
+    row <- traps[r,]
+    gen.w <- generateWeather(modelNames = 'Spruce_Budworm_Biology',
+                             fromYr = yr, toYr = yr, id = row$Location,
+                             latDeg = row$Latitude, longDeg = row$Longitude)
+    gw <- gen.w[[1]]
+    dates <- sapply(1:nrow(gw), function(rw) {
+      row2 <- gw[rw,]
+      year <- row2$Year
+      month <- str_pad(as.character(row2$Month), width = 2, pad = '0', side = 'left')
+      day <- str_pad(as.character(row2$Day), width = 2, pad = '0', side = 'left')
+      date <- paste(c(year, month, day), collapse = '-')
+    })
+    dates <- as.Date(dates)
+    rg1 <- range(which(gw$MaleFlight > 0))
+    rg2 <- range(which(gw$MaleAdult > 0))
+    start1 <- dates[rg1[1]]
+    end1 <- dates[rg1[2]]
+    start2 <- dates[rg2[1]]
+    end2 <- dates[rg2[2]]
+    df <- data.frame('Location' = row$Location, 'Year' = yr, 
+                     'f_sDate' = start1, 'f_eDate' = end1,
+                     'a_sDate' = start2, 'a_eDate' = end2)
+    return(df)
   })
-  dates <- as.Date(dates)
-  rg1 <- range(which(gw$MaleFlight > 0))
-  rg2 <- range(which(gw$MaleAdult > 0))
-  start1 <- dates[rg1[1]]
-  end1 <- dates[rg1[2]]
-  start2 <- dates[rg2[1]]
-  end2 <- dates[rg2[2]]
-  df <- data.frame('Location' = row$Location, 'Year' = row$Year, 
-                   'f_sDate' = start1, 'f_eDate' = end1,
-                   'a_sDate' = start2, 'a_eDate' = end2)
-  return(df)
+  pheno.df <- bind_rows(pheno.lst)
+  pheno.df$Year <- yr
 })
-pheno.df <- bind_rows(pheno.lst)
-pheno.df$Location <- factor(pheno.df$Location, levels = loc.ord)
+pheno.yr.df <- bind_rows(pheno.yr.lst)
+pheno.yr.df$Location <- factor(pheno.yr.df$Location, levels = loc.ord)
 
 ggplot(data = pheno.df) + 
   geom_segment(aes(x = a_sDate, xend = a_eDate, y = factor(Location), yend = factor(Location)),
